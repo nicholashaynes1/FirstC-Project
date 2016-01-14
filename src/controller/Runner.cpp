89cf47d1f5fcc74642e21d997cb11f875a402888
nicholashaynes1 <nicholashaynes1@gmail.com>
2016-01-14 13:54:58 -0700

    This simply prints to the console.

---
+++ src/controller/Runner.cpp
@@ -0,0 +1,20 @@
+/*
+ * Runner.cpp
+ *
+ *  Created on: Jan 12, 2016
+ *      Author: nhay7834
+ */
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	cout << "This is how we speak to the console in C++" << endl;
+	//this is how to print in C++
+
+
+    return 0;
+}
+
+
